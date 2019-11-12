0b33ca241d33fabcf574c108c2f896846c496c4a
Kirpalsinh9 <kirpalsinhji976@yahoo.com>
2019-11-11 19:14:42 -0500

    Improved Contract and added  initial goal

--- Desktop/WIL/Main_Dapp/Smart_Contracts/Sponsors.sol
+++ Desktop/WIL/Main_Dapp/Smart_Contracts/Sponsors.sol
@@ -17,11 +17,11 @@ contract Sponsors
     mapping(address => sponsor) public sponsorsDB;
     Rewards public rewardsContract;
     
-    constructor(address _rewardsContract) public {
+    constructor(address _rewardsContract) public payable {
         rewardsContract=Rewards(_rewardsContract);
-        if(!rewardsContract.isMinter(sponsorshipContractAddress)) {
-            rewardsContract.addMinters(sponsorshipContractAddress);
-        }
+        // if(!rewardsContract.isMinter(sponsorshipContractAddress)) {
+        //     rewardsContract.addMinters(sponsorshipContractAddress);
+        // }
        
        
        sponsorshipContractAddress = address(this);
@@ -34,24 +34,54 @@ contract Sponsors
     }
     function addSponsor(address _sponsor) public {
         require(!sponsorsDB[tx.origin].issponsor, "It's already added");
+        rewardsContract.addMinters(_sponsor);
         sponsorsDB[_sponsor].issponsor = true;
         
     }
     
     function removeSponsor(address _sponsor) public {
         require(sponsorsDB[_sponsor].issponsor, "It's already removed");
+        rewardsContract.removeMinters(_sponsor);
         sponsorsDB[_sponsor].issponsor = false;
     }
     
-    function sponsoring() public isAuthorized payable {
-        sponsorsDB[tx.origin].sponsorship += msg.value;
+    function sponsoring(uint256 _amt) public isAuthorized payable {
+        sponsorsDB[tx.origin].sponsorship += _amt;
     }
     
     function triggertokens() public payable {
-       rewardsContract.trigger();
+       rewardsContract.trigger() ;
     }
     
     function withdraw() public payable {
         rewardsContract.withdraw();
     }
+}
+
+contract Sponsor2 {
+    Sponsors database;
+    uint256  public initialgoal;
+    uint256 public totalsponsorship;
+    uint256 amt;
+    constructor(address _database, uint256 _initialgoal) public {
+        database = Sponsors(_database);
+        initialgoal = _initialgoal ;
+        totalsponsorship = 0;
+    }
+    
+    function addMintersfrom2(address _mins) public{
+        database.addSponsor(_mins);
+    }
+    
+    function removeMintersfrom2(address _mins) public{
+        database.removeSponsor(_mins);
+    }
+    
+    function sponsoringfrom2() public payable{
+        require(totalsponsorship<=initialgoal,"Sponsorship is done.");
+        amt = msg.value ;
+        totalsponsorship += amt;
+        database.sponsoring(amt);
+    }
+    
 }
